bdf36ff7584820ba21fc7fd2bec94546c81c9c4f
Zifeng22 <linmonzfon@gmail.com>
2026-06-11 00:18:52 +0800

    main debug

--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class main {
 
@@ -10,5 +12,10 @@ public class main {
 		SpringApplication.run(main.class, args);
 		
 	}
+	@PostConstruct
+    public void testEnv() {
+        System.out.println("DB_URL = " + System.getenv("DB_URL"));
+        System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
+    }
 
 }
