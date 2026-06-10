4b1727b21cde342587ba84722461f31d9214da2e
Zifeng22 <linmonzfon@gmail.com>
2026-06-11 00:26:03 +0800

    properties open

--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -3,8 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 public class main {
 
@@ -12,10 +10,4 @@ public class main {
 		SpringApplication.run(main.class, args);
 		
 	}
-	@PostConstruct
-    public void testEnv() {
-        System.out.println("DB_URL = " + System.getenv("DB_URL"));
-        System.out.println("DB_USERNAME = " + System.getenv("DB_USERNAME"));
-    }
-
 }
